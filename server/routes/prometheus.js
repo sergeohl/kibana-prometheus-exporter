9d28a99e09332fdf73dec8f21bf5a78483b7eaae
Serge Ohl <sohl@coveo.com>
2018-05-23 15:48:29 -0400

    Add ssl verification

--- server/routes/prometheus.js
+++ server/routes/prometheus.js
@@ -1,40 +1,69 @@
-
 const request = require('request');
 const formatter = require('./formatter');
+const rejectUnauthorized = true;
 
 export default function (server) {
 
+  if (server.info.protocol == 'https') {
+    rejectUnauthorized = getVerificationMode();
+  }
   server.route({
     path: '/_prometheus/metrics',
     method: 'GET',
     handler(req, reply) {
 
-      getMetrics( server.info.protocol
-                , server.info.host
-                , server.info.port
-                , function getMetricsCallback(error, info) {
+      getMetrics(server.info.protocol,
+        server.info.host,
+        server.info.port,
+        rejectUnauthorized,
+        function getMetricsCallback(error, info) {
 
-        if (error) {
-          reply(error);
-          return
-        }
+          if (error) {
+            reply(error);
+            return
+          }
 
-        reply(formatter(info)).type('text/plain').encoding('binary');
-      });
+          reply(formatter(info)).type('text/plain').encoding('binary');
+        });
     }
   });
 }
 
-function getMetrics(protocol, host, port, callback) {
+function getMetrics(protocol, host, port, rejectUnauthorized = true, callback) {
 
   const url = `${protocol}://${host}:${port}/api/status`;
 
-  request(url, function (error, res, body) {
-    if (error) {
-      callback(error);
-      return;
-    }
+  request({
+      url: url,
+      method: 'GET',
+      rejectUnauthorized: rejectUnauthorized
+    },
+    function (error, res, body) {
+      if (error) {
+        callback(error);
+        return;
+      }
 
-    callback(null, JSON.parse(body));
-  });
+      callback(null, JSON.parse(body));
+    });
+}
+
+function getVerificationMode() {
+  const verificationMode = config.get('elasticsearch.ssl.verificationMode');
+
+  switch (verificationMode) {
+    case 'none':
+      rejectUnauthorized = false;
+      break;
+    case 'certificate':
+      rejectUnauthorized = true;
+      break;
+    case 'full':
+      rejectUnauthorized = true;
+      break;
+    default:
+      throw new Error(`Unknown ssl verificationMode: ${verificationMode}`);
+  }
+
+  return rejectUnauthorized;
 }
